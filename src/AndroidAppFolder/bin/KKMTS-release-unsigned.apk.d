d9bde86553a97679a666e2238e1470b902d54d0b
Mahesh <maheshs@turanto.com>
2017-03-24 11:36:35 +0000

    include bin

---
+++ src/AndroidAppFolder/bin/KKMTS-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+D:\GenApps\KKMTS\AndroidAppFolder\bin\KKMTS-release-unsigned.apk : \
+D:\GenApps\KKMTS\AndroidAppFolder\bin\KKMTS.ap_ \
+D:\GenApps\KKMTS\AndroidAppFolder\bin\classes.dex \
+D:\GenApps\KKMTS\AndroidAppFolder\libs\cordova-2.5.0.jar \
